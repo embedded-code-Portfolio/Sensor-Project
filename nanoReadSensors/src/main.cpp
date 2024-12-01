e115148f4c53c977eef955d7d6fa82183167f440
kensus <ekeino@gmail.com>
2024-12-02 00:12:06 +0200

    Added a BMP280 sensor using SPI protocal

--- nanoReadSensors/src/main.cpp
+++ nanoReadSensors/src/main.cpp
@@ -1,54 +1,119 @@
 // Interfacing nano with sensors
-
+// inlude arduino libraries
 #include <Arduino.h>
-#include <Wire.h> // I2C lib
-// #include <Adafruit_LiquidCrystal.h>
+#include <Wire.h>
+#include <SPI.h>
+
+// Component libraries
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_BMP280.h>
 
 // Function declarations
-void deviceAddress();
-void lcdDisplay();
+void scanAvailableAddress();
+void readBmp280Sensor();
 
 // Global variables
 #define DHTPIN 6 // connected to pin 2
 #define DHTTYPE DHT11
 
-// set lcd address and screen size
+#define BMP_SCL 13
+#define BMP_SDO 12
+#define BMP_SDA 11
+#define BMP_CSB 10
+
+// Set lcd address and screen size
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+Adafruit_BMP280 bmp(BMP_CSB, BMP_SDA, BMP_SDO, BMP_SCL);
 
 // setup
 void setup()
 {
   Wire.begin(); // initilize i2c
   Serial.begin(115200);
-  lcd.init();      // initilize the lcd
-  lcd.backlight(); // tunning on the lcd back light
+
+  lcd.init();      // Initilize the lcd
+  lcd.backlight(); // Turning on the lcd back light
   lcd.setCursor(0, 0);
-  lcd.print("I2C connection ");
+  lcd.print("I2C bus ");
+
   delay(3000);
   lcd.clear();
 
   // scan if connections are Ok.
-  deviceAddress();
-  lcdDisplay();
-  delay(500);
+  scanAvailableAddress();
+
+  delay(1000);
 }
 //-------------------------------------------------------------
 void loop()
 {
+  // display BMP280 results on lcd
+  readBmp280Sensor();
+  delay(3000);
+  lcd.clear();
 
-  delay(2000);
+  delay(1000);
 }
 //----------------------------------------------------------------------
 // Dispay on lcd
 
-void lcdDisplay()
+void readBmp280Sensor()
 {
+  lcd.setCursor(0, 0);
+  lcd.print("BMP280 on SPI");
+  delay(3000);
+  lcd.clear();
+
+  if (!bmp.begin())
+  {
+    Serial.println("sensor not found ");
+    lcd.setCursor(0, 0);
+    lcd.print("no sensor");
+  }
+  // read sensor data
+
+  float temperature = bmp.readTemperature();
+  Serial.print("Temp: ");
+  Serial.print(temperature);
+  Serial.print(" *C");
+  Serial.println();
+
+  float pressure = bmp.readPressure();
+  // 1 Pa = 0.0001450377 psi
+  float psi = pressure * 1.450377E-4;
+  Serial.print("psi: ");
+  Serial.print(psi);
+  // Serial.print(" Pa");
+  Serial.println();
+
+  float altitude = bmp.readAltitude();
+  Serial.print("Alt: ");
+  Serial.print(altitude);
+  Serial.print(" m");
+  Serial.println();
+
+  // Display on LCD
+  lcd.setCursor(0, 0);
+  lcd.print("T ");
+  lcd.print(temperature);
+  lcd.setCursor(7, 0);
+  lcd.print(",");
+
+  lcd.setCursor(9, 0);
+  lcd.print("Alt ");
+  lcd.print(altitude);
+
+  lcd.setCursor(0, 1);
+  lcd.print("Psi ");
+  lcd.print(psi);
+
+  delay(3000);
 }
 
 // put function definitions here:
 
-void deviceAddress()
+void scanAvailableAddress()
 {
   // variables
   byte errorValue;          // Error code
