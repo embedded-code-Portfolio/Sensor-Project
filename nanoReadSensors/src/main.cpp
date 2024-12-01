30278d1253a7e391f888e2aa3732eac897472d1c
kensus <ekeino@gmail.com>
2024-12-01 12:11:43 +0200

    Added a 16x2 lcd display using I2C protocal

--- nanoReadSensors/src/main.cpp
+++ nanoReadSensors/src/main.cpp
@@ -1,23 +1,49 @@
+// Interfacing nano with sensors
+
 #include <Arduino.h>
-//  Interfacing nano with sensors
 #include <Wire.h> // I2C lib
+// #include <Adafruit_LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
-// put function declarations here:
+// Function declarations
 void deviceAddress();
+void lcdDisplay();
 
+// Global variables
+#define DHTPIN 6 // connected to pin 2
+#define DHTTYPE DHT11
+
+// set lcd address and screen size
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// setup
 void setup()
 {
   Wire.begin(); // initilize i2c
-  Serial.begin(9600);
-  while (!Serial)
-    delay(10);
+  Serial.begin(115200);
+  lcd.init();      // initilize the lcd
+  lcd.backlight(); // tunning on the lcd back light
+  lcd.setCursor(0, 0);
+  lcd.print("I2C connection ");
+  delay(3000);
+  lcd.clear();
 
+  // scan if connections are Ok.
   deviceAddress();
+  lcdDisplay();
+  delay(500);
 }
-
+//-------------------------------------------------------------
 void loop()
 {
-  // put your main code here, to run repeatedly:
+
+  delay(2000);
+}
+//----------------------------------------------------------------------
+// Dispay on lcd
+
+void lcdDisplay()
+{
 }
 
 // put function definitions here:
@@ -61,5 +87,10 @@ void deviceAddress()
   Serial.print("Addresses found: ");
   Serial.print(addressCount);
   Serial.println();
+  lcd.setCursor(0, 0);
+  lcd.print("Device address ");
+  lcd.setCursor(0, 1);
+  lcd.print(addressCount);
+  delay(3000);
+  lcd.clear();
 }
-
